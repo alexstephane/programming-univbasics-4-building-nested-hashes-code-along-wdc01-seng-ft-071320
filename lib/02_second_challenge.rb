954975226a7adc4a177f8710acd1fa52752351b2
Alex Ngundji <alexngundji@gmail.com>
2020-06-15 15:10:26 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,4 +1,17 @@
 def second_challenge
+     :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
  
 
   
