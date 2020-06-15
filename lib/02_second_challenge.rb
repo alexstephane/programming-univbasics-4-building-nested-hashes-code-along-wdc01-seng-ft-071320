6e20531e9f5fc184f3868c7eba411be601aebd2b
Alex Ngundji <alexngundji@gmail.com>
2020-06-15 15:11:45 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,13 @@
 def second_challenge
-     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
- 
+  epic_tragedy = {
+  }  :montague => {
+      #your key/value pairs here!
+      :patriarch =>{},:matriarch =>{},:hero [:hero_friends
+      }, 
+    :capulet => {
+      #your key/value pairs here!
+      :patriarch,:matriarch,:heroine,[:heroine_friends]
+      }
 
   
 end
